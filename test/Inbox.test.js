5e3ac1b1782f0dafdac801c51feb84bae8cafe14
NiftyGems <info@niftygems.art>
2021-07-21 00:36:54 +0100

    Asserting deployment and initial message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,10 +3,31 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const internal = require("stream");
 const Web3 = require("web3");
+const { interface, bytecode } = require("../compile");
+
+//create an instance of web3
 const web3 = new Web3(ganache.provider());
 
-// This is a simple test from lesson 46:
+//declare accounts variable
+let accounts;
 
+beforeEach(async () => {
+  //get a list of accounts. Use async await instead of promises before assigning list of accounts to 'accounts' variable
+  accounts = await web3.eth.getAccounts();
+  //use one of these accounts to deploy our contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
+});
+
+// This is a simple test from lesson 46:
+// he uses beforeEach somewhere here. I might want to review this lesson
 /*
 class Car {
   park() {
