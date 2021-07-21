b96c1d5dddb4c61203a3960727e85939dd119400
NiftyGems <info@niftygems.art>
2021-07-21 23:24:31 +0100

    Testing message updates

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -10,19 +10,33 @@ const web3 = new Web3(ganache.provider());
 
 //declare accounts variable
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   //get a list of accounts. Use async await instead of promises before assigning list of accounts to 'accounts' variable
   accounts = await web3.eth.getAccounts();
-  //use one of these accounts to deploy our contract
+  //use one of these accounts to deploy our contract.
+  //The following code is broken down line by line in lesson 51
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ["Hi there!"] })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
+  // testing that the contract was deployed to an address
   it("deploys a contract", () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+  // testing that initial default message is passed
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
+  // testing that setMessage works
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("New Message!").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "New Message!");
   });
 });
 
